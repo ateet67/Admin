d5428d045f6a40d2dfc3c82c2e55be3a3379c9d4
attet <sanjay.n@shaligraminfotech.com>
2023-09-19 16:34:25 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { JwtInterceptor } from './core/helpers/jwt.interceptor';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 export function createTranslateLoader(http: HttpClient): any {
+  
   return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 }
 
